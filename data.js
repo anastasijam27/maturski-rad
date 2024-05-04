86a2768b19fc86fad75aefb65065404c71637c12
Anastasija Milosevic <anastasijamilosevic2005@gmail.com>
2024-05-04 18:29:12 +0200

    commit

---
+++ data.js
@@ -0,0 +1,243 @@
+sneakers = [
+    {
+      id: 1,
+      name: "Nike Air Max 90",
+      brand: "Nike",
+      price: 21999,
+      color: ["Grey", "Black"],
+      sizeAvailable: [40, 44, 45],
+      imageURL: "https://www.buzzsneakers.rs/files/images/slike-proizvoda/media/FN7/FN7810-001/images/FN7810-001.jpg",
+      gender: "Male"
+    },
+    {
+      id: 2,
+      name: "Nike Court Vision Alta",
+      brand: "Nike",
+      price: 11899,
+      color: ["White"],
+      sizeAvailable: [36, 37, 38, 39, 40, 41, 42],
+      imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/HR0/HR0063/images/thumbs_900/HR0063_900_900px.jpg",
+      gender: "Female"
+    },
+    {
+      id: 3,
+      name: "Jordan 1 Retro High",
+      brand: "Nike",
+      price: 20999,
+      color: ["White", "Blue"],
+      sizeAvailable: [38],
+      imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/555/555088-134/images/thumbs_900/555088-134_900_900px.jpg",
+      gender: "Female"
+    },
+    {
+        id: 4,
+        name: "Nike SB Force 58 Premium",
+        brand: "Nike",
+        price: 10499,
+        color: ["White", "Black", "Green"],
+        sizeAvailable: [42, 43, 44, 45],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/DV5/DV5476-002/images/thumbs_900/DV5476-002_900_900px.jpg",
+        gender: "Unisex"
+      },
+      {
+        id: 5,
+        name: "Vans Old Skool",
+        brand: "Vans",
+        price: 9499,
+        color: ["Black"],
+        sizeAvailable: [36, 38, 39, 40, 41, 42, 43, 44, 45, 46],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/VN0/VN000D3HY28/images/thumbs_900/VN000D3HY28_900_900px.jpg",
+        gender: "Unisex"
+      },
+      {
+        id: 6,
+        name: "Converse Chuck Taylor All Star",
+        brand: "Converse",
+        price: 11499,
+        color: ["Black"],
+        sizeAvailable: [37, 39, 41, 42],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/561/561675C/images/thumbs_900/561675C_900_900px.jpg",
+        gender: "Female"
+      },
+      {
+        id: 7,
+        name: "New Balance 530",
+        brand: "New Balance",
+        price: 13499,
+        color: ["White", "Blue"],
+        sizeAvailable: [42, 43, 44, 45],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/MR5/MR530QA/images/thumbs_900/MR530QA_900_900px.jpg",
+        gender: "Unisex"
+      },
+      {
+        id: 8,
+        name: "Reebok Club C 85",
+        brand: "Reebok",
+        price: 11799,
+        color: ["White"],
+        sizeAvailable: [40, 41, 42, 43, 44, 45],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/100/100074163/images/thumbs_900/100074163_900_900px.jpg",
+        gender: "Male"
+      },
+      {
+        id: 9,
+        name: "Nike Air Max TW",
+        brand: "Nike",
+        price: 20699,
+        color: ["Black", "Orange"],
+        sizeAvailable: [40, 41, 42, 43, 44, 45, 46, 47],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/FJ2/FJ2590-001/images/thumbs_900/FJ2590-001_900_900px.jpg",
+        gender: "Male"
+      },
+      {
+        id: 10,
+        name: "Adidas Campus 00s",
+        brand: "Adidas",
+        price: 12999,
+        color: ["White"],
+        sizeAvailable: [40, 41, 43, 44, 45, 46, 47],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/IF8/IF8761/images/thumbs_900/IF8761_900_900px.jpg",
+        gender: "Male"
+      },
+      {
+        id: 11,
+        name: "Nike Air Max Solo",
+        brand: "Nike",
+        price: 14199,
+        color: ["Black"],
+        sizeAvailable: [40, 41, 42, 43, 44, 45, 46, 47, 49],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/DX3/DX3666-010/images/thumbs_900/DX3666-010_900_900px.jpg",
+        gender: "Male"
+      },
+      {
+        id: 12,
+        name: "Puma CA Pro Classic",
+        brand: "Puma",
+        price: 11999,
+        color: ["White"],
+        sizeAvailable: [41, 42, 43, 44, 45, 46, 47],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/380/380190-03/images/thumbs_900/380190-03_900_900px.jpg",
+        gender: "Male"
+      },
+      {
+        id: 13,
+        name: "Adidas Handball Spezial",
+        brand: "Adidas",
+        price: 12999,
+        color: ["Green", "Brown"],
+        sizeAvailable: [41, 42, 43, 44, 45, 46, 47, 48],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/IG6/IG6183/images/thumbs_900/IG6183_900_900px.jpg",
+        gender: "Male"
+      },
+      {
+        id: 14,
+        name: "New Balance U 997R",
+        brand: "New Balance",
+        price: 13199,
+        color: ["Beige"],
+        sizeAvailable: [40, 41, 42, 43, 44, 45, 46, 47],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/U99/U997RMB/images/thumbs_900/U997RMB_900_900px.jpg",
+        gender: "Unisex"
+      },
+      {
+        id: 15,
+        name: "Asics Japan S",
+        brand: "Asics",
+        price: 9999,
+        color: ["White"],
+        sizeAvailable: [41, 42, 43, 44, 45, 46],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/120/1201A173-129/images/thumbs_900/1201A173-129_900_900px.jpg",
+        gender: "Male"
+      },
+      {
+        id: 16,
+        name: "New Balance BBW550",
+        brand: "New Balance",
+        price: 16799,
+        color: ["White", "Blue"],
+        sizeAvailable: [36, 37, 38, 39, 40, 41],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/BBW/BBW550BI/images/thumbs_900/BBW550BI_900_900px.jpg",
+        gender: "Female"
+      },
+      {
+        id: 17,
+        name: "Adidas Forum Bold Stripes",
+        brand: "Adidas",
+        price: 14399,
+        color: ["Beige"],
+        sizeAvailable: [36, 37, 38, 39, 41, 42],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/ID3/ID3154/images/thumbs_900/ID3154_900_900px.jpg",
+        gender: "Female"
+      },
+      {
+        id: 18,
+        name: "Nike Air VaporMax 2023 Flyknit",
+        brand: "Nike",
+        price: 29099,
+        color: ["Green"],
+        sizeAvailable: [36, 37, 38],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/DV6/DV6840-300/images/thumbs_900/DV6840-300_900_900px.jpg",
+        gender: "Female"
+      },
+      {
+        id: 19,
+        name: "Converse Run Star Legacy CX",
+        brand: "Converse",
+        price: 15499,
+        color: ["White"],
+        sizeAvailable: [36, 37, 38, 39, 40],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/A06/A06449C/images/thumbs_900/A06449C_900_900px.jpg",
+        gender: "Female"
+      },
+      {
+        id: 20,
+        name: "Adidas Superstar XLG",
+        brand: "Adidas",
+        price: 14399,
+        color: ["White"],
+        sizeAvailable: [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/ID4/ID4655/images/thumbs_900/ID4655_900_900px.jpg",
+        gender: "Unisex"
+      },
+      {
+        id: 21,
+        name: "Adidas Country xlg",
+        brand: "Adidas",
+        price: 8399,
+        color: ["Green"],
+        sizeAvailable: [23, 24, 25],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/IF6/IF6148/images/thumbs_900/IF6148_900_900px.jpg",
+        gender: "Kids"
+      },
+      {
+        id: 22,
+        name: "Adidas Samba",
+        brand: "Adidas",
+        price: 7799,
+        color: ["Black"],
+        sizeAvailable: [19, 20, 21, 22, 23, 24, 25, 26, 27],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/IE3/IE3680/images/thumbs_900/IE3680_900_900px.jpg",
+        gender: "Kids"
+      },
+      {
+        id: 23,
+        name: "Nike Max Aura 5",
+        brand: "Nike",
+        price: 6599,
+        color: ["Black"],
+        sizeAvailable: [18, 19, 21],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/DZ4/DZ4355-003/images/thumbs_900/DZ4355-003_900_900px.jpg",
+        gender: "Kids"
+      },
+      {
+        id: 24,
+        name: "Nike Dunk Low",
+        brand: "Nike",
+        price: 7899,
+        color: ["Black", "White"],
+        sizeAvailable: [17, 18, 19, 21, 22, 25, 26, 27],
+        imageURL: "https://www.buzzsneakers.rs/files/thumbs/files/images/slike-proizvoda/media/CW1/CW1589-100/images/thumbs_900/CW1589-100_900_900px.jpg",
+        gender: "Kids"
+      },
+  ]
+  
---
+++ script.js
@@ -0,0 +1,128 @@
+document.addEventListener("DOMContentLoaded", function(){
+    prikaz(sneakers);
+    const def = document.getElementById("default");
+    def.addEventListener("click", function(){
+        prikaz(sneakers);
+    })
+    const filterButton = document.getElementById("filter-button");
+    filterButton.addEventListener("click", function(){
+        applyFilters();
+    })
+    const clearFiltersButton = document.getElementById("clear-filters-button");
+    clearFiltersButton.addEventListener("click", function() {
+        clearFilterCheckboxes("filters");
+    });
+    const searchInput = document.querySelector('input[type="search"]');
+    searchInput.addEventListener("input", function(event){
+        const searchTerm = event.target.value.trim().toLowerCase();
+        const filteredSneakers = sneakers.filter(sneaker => sneaker.name.toLowerCase().includes(searchTerm));
+        prikaz(filteredSneakers);
+    })
+    const rastucaCena = document.getElementById("cenaRastuce");
+    rastucaCena.addEventListener("click", function() {
+        const filteredSneakers = applyFilters();
+        sortPriceLowToHigh(filteredSneakers);
+    });
+
+    const opadajucaCena = document.getElementById("cenaOpadajuce");
+    opadajucaCena.addEventListener("click", function() {
+        const filteredSneakers = applyFilters();
+        sortPriceHighToLow(filteredSneakers);
+    });
+
+    const starije = document.getElementById("modelStariji");
+    starije.addEventListener("click", function() {
+        const filteredSneakers = applyFilters();
+        sortOldest(filteredSneakers);
+    });
+
+    const novije = document.getElementById("modelNoviji");
+    novije.addEventListener("click", function() {
+        const filteredSneakers = applyFilters();
+        sortNewest(filteredSneakers);
+    });
+});
+
+function prikaz(sneakers){
+    const sContainer=document.getElementById("sneaker-container");
+    sContainer.innerHTML="";
+    sneakers.forEach(sneaker =>{
+        const kartica = document.createElement("div");
+        kartica.dataset.gender = sneaker.gender === "Male" ? "male" : sneaker.gender === "Female" ? "female" : "kids";
+        kartica.dataset.sneakerId = sneaker.id;
+        const slika = document.createElement("img");
+        slika.setAttribute("src", sneaker.imageURL);
+        const naziv = document.createElement("p");
+        naziv.innerText = sneaker.name;
+        const cena = document.createElement("p");
+        cena.innerText = sneaker.price;
+        kartica.classList.add("kartica");
+        kartica.classList.add("col-3");
+        slika.classList.add("patika-slika");
+        naziv.classList.add("patika-naslov");
+        cena.classList.add("patika-cena");
+        cena.append(" RSD");
+        kartica.appendChild(slika);
+        kartica.appendChild(naziv)
+        kartica.appendChild(cena);
+        sContainer.appendChild(kartica);
+    })
+}
+
+function applyFilters(filteredSneakers = sneakers){
+    const checkedGender = [...document.querySelectorAll('.gender-checkbox')].filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+    const checkedBrands = [...document.querySelectorAll('.brand-checkbox')].filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+    const checkedPrices = [...document.querySelectorAll('.price-checkbox')].filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+    const checkedSizes = [...document.querySelectorAll('.size-checkbox')].filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+    const checkedColors = [...document.querySelectorAll('.color-checkbox')].filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+    const filteredResults = filteredSneakers.filter(sneaker => {
+        return (
+            (!checkedGender.length || checkedGender.includes(sneaker.gender) || (sneaker.gender === "Unisex" && (checkedGender.includes("Male") || checkedGender.includes("Female")))) && 
+            (!checkedBrands.length || checkedBrands.includes(sneaker.brand)) &&
+            (!checkedPrices.length || checkedPrices.some(range => {
+                const [min, max] = range.split('-').map(parseFloat);
+                const shoePrice = parseFloat(sneaker.price);
+                return shoePrice >= min && shoePrice <= max;
+            })) &&
+            (!checkedSizes.length || checkedSizes.some(size => sneaker.sizeAvailable.includes(parseInt(size)))) &&
+            (!checkedColors.length || checkedColors.some(color => sneaker.color.includes(color)))
+        );
+    });
+    prikaz(filteredResults);
+    if (filteredResults.length === 0) {
+        return [];
+    }
+
+    return filteredResults;
+}
+
+function clearFilterCheckboxes(filterSectionId) {
+    var filterSection = document.getElementById(filterSectionId);
+    if (filterSection) {
+        var checkboxes = filterSection.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(function(checkbox) {
+            checkbox.checked = false;
+        });
+    }
+    prikaz(sneakers);
+}
+
+function sortPriceLowToHigh(filteredSneakers) {
+    const sortedSneakers = filteredSneakers.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+    prikaz(sortedSneakers);
+}
+
+function sortPriceHighToLow(filteredSneakers) {
+    const sortedSneakers = filteredSneakers.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+    prikaz(sortedSneakers);
+}
+
+function sortOldest(filteredSneakers) {
+    const sortedSneakers = filteredSneakers.sort((a, b) => a.id - b.id);
+    prikaz(sortedSneakers);
+}
+
+function sortNewest(filteredSneakers) {
+    const sortedSneakers = filteredSneakers.sort((a, b) => b.id - a.id);
+    prikaz(sortedSneakers);
+}
